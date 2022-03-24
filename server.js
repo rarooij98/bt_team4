e886626957c6a9df6687e0d9c2eb8d253a5473f0
Mitchel-DS <mitchel.staal@outlook.com>
2022-03-24 23:30:04 +0100

    added folder structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+
+//engine kiezen
+const engine = require('handlebars')
+app.engine('handlebars', engine)
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+//styles inladen
+app.use(express.static(__dirname + '/static'))
+
+//laat home form zien
+app.get('/', (req, res) => {
+    res.render('home')
+    })
+
+//Server luistert op poort 8080
+app.listen(port, () => {
+    console.log('Server running on localhost:3000')
+})
