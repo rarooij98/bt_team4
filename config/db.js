a2d468f018c64f4ad970da1acdd5c1050a01440a
Sanne Kes <sannerosa.kes@gmail.com>
2022-03-25 17:31:16 +0100

    db connected en pagina live

--- config/db.js
+++ config/db.js
@@ -14,39 +14,3 @@ const connectDB = () => {
   }
  }
  module.exports = connectDB
-
-const port = process.env.port || 8000
-
-require('dotenv').config()
-
-
-const connectDB = require('./config/db')
-const res = require('express/lib/response')
-connectDB();
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
-app.use(bodyParser.urlencoded({ extended: true   }));
-
-app.use(express.static(__dirname + '/static'));
-
-app.use(express.urlencoded({ extended: true}));
-
-
-app.get('/', (req, res) => {
-    res.render('home')
-   })
-
-
-// functie om te connecten naar db //
-
-
-
-app.listen(port, () => {
-    console.log(`Example app listening on localhost:${port}`)
-  })
-  
-  
-  
