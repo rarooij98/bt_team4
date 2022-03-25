776cf0cc2eeaec24d3f7b5fa17b3d49ae6107ff5
Sanne Kes <sannerosa.kes@gmail.com>
2022-03-25 16:26:26 +0100

    setup handlebars

---
+++ db/db.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const {engine} = require('express-handlebars')
+const bodyParser = require('body-parser')
+const app = express()
+const router = express.Router()
+
+
+const port = proCocess.env.port || 8000
+
+require('dotenv').config()
+
+
+const connectDB = require('./config/db')
+const res = require('express/lib/response')
+connectDB();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: true   }));
+
+app.use(express.static(__dirname + '/static'));
+
+app.use(express.urlencoded({ extended: true}));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+   })
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on localhost:${port}`)
+  })
+  
+  
+  
