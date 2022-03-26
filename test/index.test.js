4603a07d7a7be3f179b28d3a6f14d9b7dbf92dbc
Robin van Rooij <robinvanrooij@live.nl>
2022-03-26 12:56:41 +0100

    werken aan tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,27 @@
 // hier komt de Mocha/Chai code waarmee index.js getest kan worden
+
+// import de file die getest moet worden
+const math = require('../index.js');
+
+// requirements voor mocha en chai
 const assert = require('assert');
 const should = require('chai').should();
+const expect = require('chai').expect;
 
-// om te testen of er een ander bericht word weergeven als er geen matches zijn gevonden:
+// 1: testen of er een ander bericht word weergeven als er geen matches zijn gevonden
 // const title  = (scholen.length == 0) ? "Er zijn geen matches gevonden" : "Matches:";
 describe('No Matches Test', () => {
- it('should return string: "Er zijn geen matches gevonden"', () => {
-    const scholen = []
-    scholen.length.should.equal(0);
+    
+    it('should return true: "Er zijn geen matches gevonden"', () => {
+        const scholen = []
+        const result = (scholen.length == 0);
+        expect(result).to.equal(true);
     });
+    
+    it('should return false: "Matches:"', () => {
+        const scholen = ["item"]
+        const result = (scholen.length == 0);
+        expect(result).to.equal(false);
+    });
+    
 });
-
