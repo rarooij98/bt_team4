428acae14102fb7b58f9ca09bc1ef1ca4b2f8490
Robin van Rooij <robinvanrooij@live.nl>
2022-03-30 15:21:44 +0200

    eigen styling afgestemd op algemene styling bestand

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const {engine} = require('express-handlebars')
-const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
+const bodyParser = require('body-parser')
 const multer = require('multer')
 const upload = multer({dest: 'static/uploads/'})
 const mongoose = require('mongoose')
 const Keuze = require("./models/user")
-const {redirect} = require('express/lib/response')
 
 let db = null
 const port = process.env.port || 8000
@@ -58,15 +57,15 @@ app.get('/matches', async (req, res) => {
   const query = {...queryLocatie, ...queryNiveau, ...queryOnderwerp};
   const options = {sort: {name: 1}};
   
-  // We moeten het schema specificeren, ook al verwijzen we naar de bestaande verzameling in DB:
+  // we moeten het schema specificeren, ook als we verwijzen naar de bestaande verzameling in DB:
   const Schema = mongoose.Schema;  
-  const scholen = mongoose.model("scholen", new Schema({
+  const scholen = mongoose.model('scholen', new Schema({
     name: String,
     locatie: String,
     niveau: String,
     onderwerpen: Array
-    }), "scholen");
-
+    }), 'scholen');
+  
   const filtered = await scholen.find(query, null, options).lean();
   const title = (scholen.length == 0) ? "Er zijn geen matches gevonden" : "Matches:";
   res.render('matches', {title: title, scholen: filtered});
