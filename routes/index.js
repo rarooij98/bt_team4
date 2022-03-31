31b3407bb691c6dde375bd29ab40f759623e2ba7
Mitchel-DS <mitchel.staal@outlook.com>
2022-03-31 14:48:33 +0200

    added chat view

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const home = require('./home');
 const login = require('./login');
 const register = require('./register');
+const chat = require('./chat');
 
 router.use('/', home);
 router.use('/login', login);
 router.use('/register', register);
+router.use('/chat', chat);
 
 module.exports = router;
