06037388aed1de3d10ab6cc455e5cf83d56e93a3
Mitchel-DS <mitchel.staal@outlook.com>
2022-03-31 13:51:36 +0200

    removed loginController.js file

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,14 +1,12 @@
-// const User = require('../models/User');
-// in de controller kun je de models requiren die je evt nodig hebt
+// // const User = require('../models/User');
 
-const homeView = (req, res) => {
-    res.render("home", {
-    } );
-}
-module.exports =  {
-    homeView
-};
 
-// wanneer een gebruiker een verzoek indient, wordt dit naar de controller gestuurd
-// de controller heeft via de model toegang tot de database
-// dan geeft de controller de respons door aan de view
+// const homeView = (req, res) => {
+//     res.render("home", {
+//     } );
+// }
+// module.exports =  {
+//     homeView
+// };
+
+
