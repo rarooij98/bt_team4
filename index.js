33a1b3b658dc62a3cb6176f159b8034c051d33eb
Mitchel-DS <mitchel.staal@outlook.com>
2022-04-01 17:40:54 +0200

    setup, installed socket.io

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const express = require('express');
 const routes = require('./routes')
 const app = express()
-const {engine} = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const connectDB = require('./config/db')
 const port = process.env.port || 8000
 const bodyParser = require('body-parser');
-const session = require('express-session');
-
+const socketio = require('socket.io');
 require('dotenv').config()
 connectDB();
 
 // session
+const session = require('express-session');
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
 
+// socket.io
+const ioPort = process.env.ioPort || 3000
+const http = require('http');
+const server = http.createServer(app);
+const io = socketio(server);
+
 // handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // static folder
 app.use(express.static(__dirname + '/static'));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on localhost:${port}`)
-  })
+  console.log(`Example app listening on localhost:${port}`)
+});
+
+server.listen(ioPort, () => {
+  console.log(`Socket test app listening on localhost:${ioPort}`)
+});
