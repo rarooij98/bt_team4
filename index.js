a3145e09cd8bb62f0e5cae49b36ba53f7ee7dbf1
Mitchel-DS <mitchel.staal@outlook.com>
2022-04-01 15:30:13 +0200

    cleaned index.js

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ const express = require('express');
 const routes = require('./routes')
 const app = express()
 const {engine} = require('express-handlebars')
-require('dotenv').config()
 const connectDB = require('./config/db')
 const port = process.env.port || 8000
-require('dotenv').config()
-connectDB();
 const bodyParser = require('body-parser');
 const session = require('express-session');
+
+require('dotenv').config()
+connectDB();
+
+// session
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
 
-
+// handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.use(bodyParser.urlencoded({ extended: true}));
+// static folder
 app.use(express.static(__dirname + '/static'));
 app.use(express.urlencoded({ extended: true}));
 
+// routes
 app.use('/', routes);
 
 app.listen(port, () => {
