9495152e0b39f0fda1de107c546fb0b7d806e4c9
Robin van Rooij <robinvanrooij@live.nl>
2022-04-03 00:05:21 +0200

    test geschreven voor het posten van keuze formulier

--- test/index.test.js
+++ test/index.test.js
@@ -24,5 +24,31 @@ describe('School', () => {
             });
         });
     });
+    
+/*
+* Test the /POST route*
+*/
+    
+    describe('/POST keuze', () => {
+        it('it should POST keuze', (done) => {
+        
+        let keuze = {
+        locatie: 'Amsterdam',
+        niveau: 'HBO',
+        onderwerp: 'Design en Creatie'
+        }
+        
+        chai.request(app)
+        .post('/form')
+        .send(keuze)
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.keuze.should.have.property('locatie');
+            res.body.keuze.should.have.property('niveau');
+            res.body.keuze.should.have.property('onderwerp');
+            done();
+            });
+        });
+    });
 
 });
