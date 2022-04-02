d006d1068b7c7a7cbf151ad213661c734b960187
Robin van Rooij <robinvanrooij@live.nl>
2022-04-03 00:03:34 +0200

    test geschreven voor get request vd matches

--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,28 @@
-// hier komt de Mocha/Chai code waarmee index.js getest kan worden
+//process.env.NODE_ENV = 'test';
 
-// import de file die getest moet worden
-const math = require('../index.js');
+const app = require('../index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
 
-// requirements voor mocha en chai
-const assert = require('assert');
-const should = require('chai').should();
-const expect = require('chai').expect;
+chai.use(chaiHttp)
 
-/** TESTEN IS NOG EEN WORK IN PROGRESS **/
-// hieronder is een test uitprobeersel
+describe('School', () => {
 
-// 1: testen of er een ander bericht word weergeven als er geen matches zijn gevonden
-// const title  = (scholen.length == 0) ? "Er zijn geen matches gevonden" : "Matches:";
-describe('No Matches Test', () => {
-    
-    it('should return true: "Er zijn geen matches gevonden"', () => {
-        const scholen = []
-        const result = (scholen.length == 0);
-        expect(result).to.equal(true);
+/*
+* Test the /GET route*
+*/
+
+    describe('/GET school', () => {
+        it('it should GET schools', (done) => {
+            chai.request(app)
+            .get('/matches')
+            .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            done();
+            });
+        });
     });
-    
-    it('should return false: "Matches:"', () => {
-        const scholen = ["item"]
-        const result = (scholen.length == 0);
-        expect(result).to.equal(false);
-    });
-    
+
 });
