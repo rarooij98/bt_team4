b5defa957aa0a8c428482861c3d0f9ba30132986
Robin van Rooij <robinvanrooij@live.nl>
2022-04-03 16:16:26 +0200

    test: get login formulier

--- test/index.test.js
+++ test/index.test.js
@@ -1,54 +1,43 @@
 const app = require('../index');
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const should = chai.should();
-
+var expect = require('chai').expect
 chai.use(chaiHttp)
 
-describe('School', () => {
-
 /*
-* Test the /GET route*
+* Test the /GET route *
 */
-
-    describe('/GET school', () => {
-        it('it should GET schools', (done) => {
-            chai.request(app)
-            .get('/matches')
-            .end((err, res) => {
-            res.should.have.status(200);
-            done();
-            });
+describe('/GET login formulier', () => {
+    it('it should GET the login view', (done) => {
+        chai.request(app)
+        .get('/login')
+        .end((err, res) => {
+        expect(res.status).to.be.eq(200);
+        done();
         });
     });
-    
+});
+
 /*
 * Test the /POST route*
-
-* Error message: *
-* Timeout of 10000ms exceeded. For async tests and hooks, ensure "done()" is called; if returning a Promise, ensure it resolves *
 */
-    
-    describe('/POST keuze', () => {
-        it('it should POST keuze', (done) => {
-        
+describe('/POST keuze', () => {
+    it('it should POST keuze', (done) => {
         let keuze = {
             locatie: 'test',
             niveau: 'test',
             onderwerp: 'test'
-            }
-        
+        }
         chai.request(app)
         .post('/form')
         .send(keuze)
         .end((err, res) => {
-            res.should.have.status(200);
-            res.body.keuze.should.have.property('locatie');
-            res.body.keuze.should.have.property('niveau');
-            res.body.keuze.should.have.property('onderwerp');
+            expect(res.status).to.be.eq(200);    
+            expect(keuze).to.have.property('locatie');
+            expect(keuze).to.have.property('niveau');
+            expect(keuze).to.have.property('onderwerp');
             done();
-            });
         });
     });
-
 });
