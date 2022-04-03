44d264b043a86eb32c48d499d38ca235a12f7bbd
SBindels <bindelssjoerd@gmail.com>
2022-04-03 12:10:49 +0200

    added timestamps + reference in user model (voor topic Sjoerd)

--- models/user.js
+++ models/user.js
@@ -15,9 +15,17 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
+},
+{
+    timestamps: true
 })
 
 const keuzeSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: User,
+    },
     locatie: String,
     niveau: String,
     onderwerp: [String]
