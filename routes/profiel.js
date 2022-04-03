f68c9f6f5b7d90e0b7470281e83ab6ca686f49a2
SBindels <bindelssjoerd@gmail.com>
2022-04-03 15:11:58 +0200

    reglog route toegevoegt in profiel

--- routes/profiel.js
+++ routes/profiel.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const profiel = require('../controllers/profiel');
+const reglog = require('../controllers/reglog');
 
 router.get('/', profiel.profiel );
 router.post('/uitloggen', reglog.uitloggen);
