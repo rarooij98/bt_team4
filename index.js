fd7f6d31701f22acc1e3b26f69c591321f4c8e9e
DennisHvA <dennis.de.graaf2@hva.nl>
2022-04-03 17:57:55 +0200

    route for when its merged

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-// fixing the redirect
-require('https').globalAgent.options.rejectUnauthorized = false
-
 const express = require('express')
 const routes = require('./routes')
 const app = express()
 const session = require('express-session');
 const {engine} = require('express-handlebars')
 const bodyParser = require('body-parser')
-
-// google token
-require('./config/google');
-
-const passport = require('passport');
-app.use(session({ secret: 'cats' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
 const router = express.Router()
-
-
 const port = process.env.port || 8000
-
 require('dotenv').config()
 
 // db
@@ -38,6 +23,18 @@ app.use(bodyParser.urlencoded({ extended: true   }));
 app.use(express.static(__dirname + '/static'));
 app.use(express.urlencoded({ extended: true}));
 
+// ---------------------------------------------------------------------
+// From here is the google login + passport
+// ---------------------------------------------------------------------
+
+require('https').globalAgent.options.rejectUnauthorized = false
+require('./config/google');
+
+const passport = require('passport');
+app.use(session({ secret: 'cats' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 function IsLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
 }
@@ -46,44 +43,28 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-// google 
 app.get('/auth/google',
   passport.authenticate('google', { scope: [ 'email', 'profile' ] }
 ));
 
-// routes when using google
 app.get('/google/callback',
   passport.authenticate('google', {
     // route when logged in
-    successRedirect: '/protected',
+    successRedirect: '/profilegoogle',
     // route when failed to login
     failureRedirect: '/auth/failure',
   })  
 )
 
-// route failed to login
 app.get('/auth/failure', (req, res) => {
   res.send('Something went wrong');
 })
 
-// route when logged in
+app.get('/profilegoogle', IsLoggedIn, (req, res) => {
+  res.render('profilegoogle');
+  console.log('logged in')
 
-// app.get('/protected', IsLoggedIn, (req, res) => {
-//   res.send(`Hello ${req.user.displayName}`);
-// });
-
-app.get('/protected', IsLoggedIn, (req, res) => {
-  res.render('protected');
 });
-
-
-// end session
-
-// app.get('/logout', (req, res) => {
-//   req.logout();
-//   req.session.destroy();
-//   res.send('goodbye');
-// })
 
 app.post('/logout', (req, res) => {
   req.logout();
@@ -93,8 +74,5 @@ app.post('/logout', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on localhost:${port}`)
-  });
-  
-  
-  
+  console.log(`Example app listening on localhost:${port}`)
+});
